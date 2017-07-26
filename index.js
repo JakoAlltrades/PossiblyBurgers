1a3ef3bf5061661cb52596102f4ced2023b32f95
JakoAlltrades <Jpriemo@gmail.com>
2017-07-26 08:42:54 -0600

    Index Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express'), 
+    pug = require('pug'),
+    path = require('path'),
+    config = require('./config');
+var app = express();
+
+app.set("view engine", "pug");
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public/')));
+
+app.get('/', function(req, res)
+       {
+    res.render('index', {title: "Homepage",
+                         config: config
+                        });
+});
+
+app.get('/:page', function(req, res)
+    {
+    res.render(req.params.page, {title: req.params.page,
+                         config: config});
+});
+
+app.listen(3000);
